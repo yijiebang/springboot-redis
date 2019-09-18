0a347e73a0ef93be59f391678bbf885b7de79a50
yijiebang <18820285401@163.com>
2019-09-18 17:37:56 +0800

    删除redis工具类，直接使用spring自带的

--- com-common/src/main/java/com/yjb/common/exception/SystemCode.java
+++ com-common/src/main/java/com/yjb/common/exception/SystemCode.java
@@ -13,6 +13,7 @@ public enum SystemCode {
     SUCCESS(1000,"成功执行"),
     NO_DATA(1260,"查询数据为空"),
     LOGIN_ERROR(1001,"用户名或密码错误"),
+    LOGINTIMER_ERROR(1003,"登录过期，请重新登录"),
     NOLOGIN_ERROR(1002,"暂未登录，无权限访问"),
     ERROR_CODEDING(1111,"代码执行出错"),
     ERROR(2222,"未知异常");
