99703d162ee8057943d09f418e5d90746b70d3ac
yijiebang <18820285401@163.com>
2019-09-23 11:26:40 +0800

    s

--- com-common/src/main/java/com/yjb/common/exception/SystemCode.java
+++ com-common/src/main/java/com/yjb/common/exception/SystemCode.java
@@ -9,13 +9,13 @@ import lombok.Setter;
  */
 
 public enum SystemCode {
-
-    SUCCESS(1000,"成功执行"),
     NO_DATA(1260,"查询数据为空"),
+    SUCCESS(1000,"成功执行"),
     LOGIN_ERROR(1001,"用户名或密码错误"),
-    LOGINTIMER_ERROR(1003,"登录过期，请重新登录"),
     NOLOGIN_ERROR(1002,"暂未登录，无权限访问"),
-    ERROR_CODEDING(1111,"代码执行出错"),
+    LOGINTIMER_ERROR(1003,"登录过期，请重新登录"),
+    REGISTER_ERROR(1004,"注册失败"),
+     ERROR_CODEDING(1111,"代码执行出错"),
     ERROR(2222,"未知异常");
 
     @Getter@Setter
